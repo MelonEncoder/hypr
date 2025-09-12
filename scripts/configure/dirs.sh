7d48e7c159c940c47b10ca0fc89215ee77a39a23
MelonEncoder <iangillette@proton.me>
2025-09-12 19:28:45 -0400

    added screenshots dir

---
+++ scripts/configure/dirs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/Apps" ]; then
+    mkdir -p "$HOME/Apps"
+    echo "Create Apps directory."
+else
+    echo "Apps directory already exists."
+fi
+
+if [ ! -d "$HOME/Pictures/Screenshots" ]; then
+	mkdir -p "$HOME/Pictures/Screenshots"
+	echo "Created Screenshots directory."
+else
+	echo "Screenshots directory already exists."
+fi
