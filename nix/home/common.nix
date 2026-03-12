c40690417527f8aac6b666e56897c0a6870ccec0
MelonEncoder <iangillette@proton.me>
2026-03-12 17:41:31 -0400

    added git config

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -5,6 +5,23 @@
   home.stateVersion = "25.11";
 
   programs.home-manager.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "MelonEncoder";
+    userEmail = "iangillette@proton.me";
+
+    signing = {
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+
+    extraConfig = {
+      core.editor = "zeditor";
+      init.defaultBranch = "main";
+      gpg.format = "ssh";
+      pull.rebase = false;
+    };
+  };
 
   xdg.configFile."nvim".source = ../../home/common/.config/nvim;
   xdg.configFile."zed".source = ../../home/common/.config/zed;
